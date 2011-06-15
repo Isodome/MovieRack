216b185a697266ff0910990c6a1a99d3c6494cb0
Dominic <Dominic.rausch@googlemail.com>
2011-06-15 23:28:16 +0200

    sorry vergessen

---
+++ WinMovieRack/WinMovieRack/Controller/Finalizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WinMovieRack.Controller.ThreadManagement;
+using WinMovieRack.Controller.Parser;
+
+namespace WinMovieRack.Controller {
+	public class Finalizer : ThreadJobMaster {
+
+		private FunctionCall fc = new FunctionCall(); 
+
+		public Finalizer() {
+			this.addJob(fc);
+		}
+
+		public override bool hasFinished(ThreadJob job) {
+			return true;
+		}
+		public override bool isFinalizingTask() {
+			return true;
+		}
+
+		public void addFunction(WorkWithoutParam w) {
+			fc.addFunction(w);
+		}
+		public void addFunction(WorkWithInt w, int i) {
+			fc.addFunction(w,i);
+		}
+		public void addFunction(WorkWithObject w, object o) {
+			fc.addFunction(w,o);
+		}
+	}
+}
---
+++ WinMovieRack/WinMovieRack/Controller/Parser/generalJobs/FunctionCall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Web;
+using System.Drawing;
+using WinMovieRack.Controller.ThreadManagement;
+
+namespace WinMovieRack.Controller.Parser
+{
+	public delegate void WorkWithoutParam();
+	public delegate void WorkWithInt(int i);
+	public delegate void WorkWithObject(object obj);
+    class FunctionCall : ThreadJob
+    {
+		private WorkWithoutParam work;
+		private WorkWithInt withInt;
+		private WorkWithObject withObj;
+
+        public void run()
+        {
+			work();
+
+        }
+		public void addFunction(WorkWithoutParam w) {
+			work += w;
+		}
+		public void addFunction(WorkWithInt w, int i) {
+			work += () => {
+				w(i);
+			};
+		}
+		public void addFunction(WorkWithObject w, object o) {
+			work += () => {
+				w(o);
+			};
+		}
+    }
+}
