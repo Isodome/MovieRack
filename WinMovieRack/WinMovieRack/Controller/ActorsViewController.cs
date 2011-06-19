ce72f1ba65bb0352c8962b563288074c12a49faa
Duky <Johannes.walter87@googlemail.com>
2011-06-19 15:57:53 +0200

    Bissi Funktionalitaet

--- WinMovieRack/WinMovieRack/Controller/ActorsViewController.cs
+++ WinMovieRack/WinMovieRack/Controller/ActorsViewController.cs
@@ -16,7 +16,11 @@ namespace WinMovieRack.Controller
         private ActorsView view;
         private ObservableCollection<WinMovieRack.GUI.MRListBoxItem> actorsList = new ObservableCollection<WinMovieRack.GUI.MRListBoxItem>();
         private ObservableCollection<WinMovieRack.GUI.MRListBoxItem> movieListToPerson = new ObservableCollection<WinMovieRack.GUI.MRListBoxItem>();
+        private ObservableCollection<WinMovieRack.GUI.MRListBoxItem> starsList = new ObservableCollection<WinMovieRack.GUI.MRListBoxItem>();
+        private ObservableCollection<WinMovieRack.GUI.MRListBoxItem> productionList = new ObservableCollection<WinMovieRack.GUI.MRListBoxItem>();
 
+        private Dictionary<int, MRListBoxItem> listBoxItems = new Dictionary<int, MRListBoxItem>();
+        private bool isload = false;
         public ActorsViewController(Controller c, SQLiteConnector db)
         {
             this.db = db;
@@ -31,19 +35,24 @@ namespace WinMovieRack.Controller
 
         public void loadCompleteActorsList()
         {
-            actorsList.Clear();
-            List<MRListData> completeMovieListData = db.getCompletePersonList(PersonEnum.gender, PersonEnum.Name);//Aus Config lesen
-            completeMovieListData.ForEach(delegate(MRListData actor)
+            if (!isload)
             {
-                BitmapImage posterBitmap = new BitmapImage();
-                posterBitmap.BeginInit();
-                posterBitmap.UriSource = new Uri(PictureHandler.getPersonPortraitPath(actor.dbItemID, PosterSize.LIST));
-                posterBitmap.CreateOptions = BitmapCreateOptions.DelayCreation;
-                posterBitmap.CacheOption = BitmapCacheOption.OnDemand;
-                posterBitmap.EndInit();
-                actorsList.Add(new MRListBoxItem(actor.dbItemID, actor.titleName, actor.yearAge.ToString(), actor.editableCharakter, posterBitmap));
-            });
-            view.listBoxActor.ItemsSource = actorsList;
+                List<MRListData> completeMovieListData = db.getCompletePersonList(PersonEnum.gender, PersonEnum.Name);//Aus Config lesen
+                completeMovieListData.ForEach(delegate(MRListData actor)
+                {
+                    BitmapImage posterBitmap = new BitmapImage();
+                    posterBitmap.BeginInit();
+                    posterBitmap.UriSource = new Uri(PictureHandler.getPersonPortraitPath(actor.dbItemID, PosterSize.LIST));
+                    posterBitmap.CreateOptions = BitmapCreateOptions.DelayCreation;
+                    posterBitmap.CacheOption = BitmapCacheOption.OnDemand;
+                    posterBitmap.EndInit();
+                    MRListBoxItem item = new MRListBoxItem(actor.dbItemID, actor.titleName, actor.yearAge.ToString(), actor.editableCharakter, posterBitmap);
+                    actorsList.Add(item);
+                    listBoxItems.Add(actor.dbItemID, item);
+                });
+                view.listBoxActor.ItemsSource = actorsList;
+            }
+            isload = true;
         }
 
         public void loadMovieListToPerson(int itemID)
@@ -61,9 +70,75 @@ namespace WinMovieRack.Controller
             view.MovieListToPerson.ItemsSource = movieListToPerson;
         }
 
+        public void loadStarsListToMovie(int itemID, int year)
+        {
+            starsList.Clear();
+            List<MRListData> starsListToMovie = db.getStarsListToMovie(itemID, year);
+            starsListToMovie.ForEach(delegate(MRListData actor)
+            {
+                BitmapImage posterBitmap = new BitmapImage();
+                posterBitmap.BeginInit();
+                posterBitmap.UriSource = new Uri(PictureHandler.getPersonPortraitPath(actor.dbItemID, PosterSize.LIST));
+                posterBitmap.EndInit();
+                starsList.Add(new MRListBoxItem(actor.dbItemID, actor.titleName, actor.yearAge.ToString(), actor.editableCharakter, posterBitmap));
+            });
+            view.SummeryStarsListbox.ItemsSource = starsList;
+        }
+
+        public void loadProductionListToMovie(int itemID, int year)
+        {
+            productionList.Clear();
+            List<MRListData> productionListToMovie = db.getProductionListToMovie(itemID, year);
+            productionListToMovie.ForEach(delegate(MRListData actor)
+            {
+                BitmapImage posterBitmap = new BitmapImage();
+                posterBitmap.BeginInit();
+                posterBitmap.UriSource = new Uri(PictureHandler.getPersonPortraitPath(actor.dbItemID, PosterSize.LIST));
+                posterBitmap.EndInit();
+                productionList.Add(new MRListBoxItem(actor.dbItemID, actor.titleName, actor.yearAge.ToString(), actor.editableCharakter, posterBitmap));
+            });
+            view.SummeryProductionListbox.ItemsSource = productionList;
+        }
+
         public GUIPerson getGUIPerson(int itemID)
         {
             return db.getPersonInfo(itemID);
+        }
+
+        public GUIMovie getGUIMovie(int itemID)
+        {
+            return db.getMovieInfo(itemID);
+        }
+
+        public void deleteItem(int id)
+        {
+            MRListBoxItem itemToDelete;
+            listBoxItems.TryGetValue(id, out itemToDelete);
+            actorsList.Remove(itemToDelete);
+
+        }
+
+        public void addItem(int id, MRListBoxItem item)
+        {
+            actorsList.Add(item);
+            listBoxItems.Add(id, item);
+        }
+
+        public void changeItem(int id, MRListBoxItem item)
+        {
+            MRListBoxItem itemToChange;
+            listBoxItems.TryGetValue(id, out itemToChange);
+            itemToChange = item;
+        }
+
+        public List<String> loadGenreList(int idMovies)
+        {
+            return db.getGenresToMovie(idMovies);
+        }
+
+        public List<String> loadLanguageList(int idMovies)
+        {
+            return db.getLanguageToMovie(idMovies);
         }
     }
 }
--- WinMovieRack/WinMovieRack/GUI/ActorsView.xaml.cs
+++ WinMovieRack/WinMovieRack/GUI/ActorsView.xaml.cs
@@ -24,12 +24,12 @@ namespace WinMovieRack.GUI
         ActorsViewController controller;
         GUIPerson personDetails;
         private BigPicture bigPicture;
+        private GUIMovie movieDetails;
         public ActorsView(ActorsViewController avc)
         {
             InitializeComponent();
             this.controller = avc;
         }
-
 
         private void listBoxActor_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -40,8 +40,8 @@ namespace WinMovieRack.GUI
                 setPersonDetails(personDetails);
                 controller.loadMovieListToPerson(selectetItem.getId);
             }
+            MovieListToPerson.SelectedIndex = 0;
         }
-
 
         private void setPersonDetails(GUIPerson personDetails)
         {
@@ -63,6 +63,68 @@ namespace WinMovieRack.GUI
             actorPoster.Source = posterBitmap;
         }
 
+        private void setMovieDetails()
+        {
+            title.Text = movieDetails.title;
+            year.Content = movieDetails.year;
+            originalTitle.Content = movieDetails.originalTitle;
+            imdbRating.Content = movieDetails.imdbRating;
+            ownRating.Content = movieDetails.personalRating;
+            runtime.Content = movieDetails.runtime;
+            lastSeen.Content = movieDetails.lastSeen;
+            // cinemaDate.Content = movieDetails.
+            lastSeen.Content = movieDetails.lastSeen;
+            //mpaa.Content = movieDetails.
+            seen.Content = movieDetails.seenCount;
+            //awards.Content = movieDetails.
+            budget.Content = movieDetails.budget;
+            boxxoffice.Content = movieDetails.boxofficeWorldwide;
+            string genreString = generateString(controller.loadGenreList(movieDetails.dbId));
+            if (genreString.Equals(""))
+            {
+                genres.Content = "No Genres";
+            }
+            else
+            {
+                genres.Content = genreString;
+            }
+
+            string languageString = generateString(controller.loadLanguageList(movieDetails.dbId));
+            if (languageString.Equals(""))
+            {
+                language.Content = "No Languages";
+            }
+            else
+            {
+                language.Content = languageString;
+            }
+        }
+
+        private String generateString(List<String> listBoxMovies)
+        {
+            String generatedString = "";
+            for (int i = 0; i < listBoxMovies.Count; i++)
+            {
+                generatedString += listBoxMovies.ElementAt(i);
+                if (i < listBoxMovies.Count - 1)
+                {
+                    generatedString += ", ";
+                }
+            }
+            return generatedString;
+        }
+
+        private void loadMovieDetails(int movieID)
+        {
+            this.movieDetails = controller.getGUIMovie(movieID);
+            setMovieDetails();
+            controller.loadProductionListToMovie(movieID, movieDetails.yearInt);
+            controller.loadStarsListToMovie(movieDetails.dbId, movieDetails.yearInt);
+        }
+
+
+
+
         private void actorPoster_MouseUp(object sender, MouseButtonEventArgs e)
         {
             bigPicture = new BigPicture();
@@ -75,6 +137,15 @@ namespace WinMovieRack.GUI
             Point screenOrigin = actorPoster.PointToScreen(origin);
             bigPicture.setOrigin(actorPoster.Source.Height, actorPoster.Source.Width, screenOrigin.X, screenOrigin.Y);
             bigPicture.fadeIn();
+        }
+
+        private void MovieListToPerson_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MRListBoxItem selectedMovieItem = (MRListBoxItem)MovieListToPerson.SelectedItem;
+            if (selectedMovieItem != null)
+            {
+                loadMovieDetails(selectedMovieItem.getId);
+            }
         }
     }
 }
