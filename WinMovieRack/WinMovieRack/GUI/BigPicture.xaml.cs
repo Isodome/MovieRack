bce0a96790cc6d6d67d39e808108c2f4677418f6
Dominic <Dominic.rausch@googlemail.com>
2011-06-20 15:57:32 +0200

    Nurnoch ein BigPicture möglich

--- WinMovieRack/WinMovieRack/GUI/BigPicture.xaml.cs
+++ WinMovieRack/WinMovieRack/GUI/BigPicture.xaml.cs
@@ -41,7 +41,6 @@ namespace WinMovieRack.GUI
 			this.Width = orgWidth;
 			this.Left = posx;
 			this.Top = posy;
-			this.Show();
 
 			Storyboard sb = new Storyboard();
 			DoubleAnimation heightAnim = new DoubleAnimation();
@@ -87,7 +86,7 @@ namespace WinMovieRack.GUI
 			posXAnim.To = (System.Windows.SystemParameters.PrimaryScreenWidth - widthAnim.To) / 2;
 			
 
-
+			this.ShowDialog();
 			sb.Begin();
 		}
         private void bigPicture_MouseLeave(object sender, MouseEventArgs e)
